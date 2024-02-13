f9ca5449f357c29b7ce2fcc42535898a82cadd5b
Danny C <35072063+dchen705@users.noreply.github.com>
2024-02-12 20:29:09 -0600

    Implement starting deal

--- rb120-129/small_programs/twenty-one/twenty_one_draft2.rb
+++ rb120-129/small_programs/twenty-one/twenty_one_draft2.rb
@@ -1,50 +1,62 @@
+module Displayable
+  def display_welcome_message
+    puts "Welcome to Twenty-One game"
+    gets
+  end
+end
+
 class Participant
-    def initialize
-        @hand = Hand.new
-    end
+  attr_reader :hand
 
-    def bust
+  def initialize(type)
+      @type = type
+      @hand = Hand.new
+  end
 
-    end
+  def draw(deck)
+    hand << deck.pop
+  end
 
-    def stay
+  def hit
 
-    end
-end
+  end
 
-class Player < Participant
+  def stay
 
-end
-
-class Dealer < Participant
-
+  end
 end
 
 class Card
-    def initialize(rank, suit)
-        @rank = rank
-        @suit = suit
-        @reveal_status = false
-    end
+  RANKS = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+  SUITS = %w(♦ ♣ ♥ ♠)
+
+  def initialize(rank, suit)
+      @rank = rank
+      @suit = suit
+      @reveal_status = false
+  end
 end
 
 class Hand
-    def initialize
-        @cards = [] # array of Card objects
-    end
 
-    def <<(card)
-        cards << card
-    end
+  attr_reader :cards
 
-    def total_value
-        sum
-        adjust_ace_value
-    end
+  def initialize
+      @cards = [] # array of Card objects
+  end
 
-    def adjust_ace_value
+  def <<(card)
+      cards << card
+  end
 
-    end
+  def total_value
+      sum
+      adjust_ace_value
+  end
+
+  def adjust_ace_value
+
+  end
 end
 
 # Class Deck?
@@ -52,23 +64,60 @@ end
 # has 52 Cards initially,
 # is an ordered list suggesting array
 # within the 21Game:
-@deck = []
-double for or nested loop to concat each suit and rank to @deck
-hmm shouldn''t both Player and Dealer have acess to Deck to hit?
-Should Deck be a mix-in module then?
-In OOP, tradeoff between loose coupling (like Player/Dealer to Deak through intermediate Game)
-tight coupling (create Deck in Game then pass into initialization of Player/Dealer?)
-but we prob doesn''t want to suggest Player has Deck....
+# @deck = []
+# double for or nested loop to concat each suit and rank to @deck
+# hmm shouldn''t both Player and Dealer have acess to Deck to hit?
+# Should Deck be a mix-in module then?
+# In OOP, tradeoff between loose coupling (like Player/Dealer to Deak through intermediate Game)
+# tight coupling (create Deck in Game then pass into initialization of Player/Dealer?)
+# but we prob doesn''t want to suggest Player has Deck....
 
-class 21Game
+class Game_21
+  include Displayable
+
+  def initialize
+    @player = Participant.new(:player)
+    @dealer = Participant.new(:dealer)
+    @deck = new_deck
+  end
+
+  def new_deck
+    deck = []
+    Card::SUITS.each do |suit|
+      Card::RANKS.each do |rank|
+        deck << Card.new(rank, suit)
+      end
+    end
+    deck.shuffle
+  end
+
+  def deal
+    2.times do
+      player.draw(deck)
+      dealer.draw(deck)
+    end
+  end
+
+  def play
     display_welcome_message
     deal
-    player_turn
-        break if bust? || stay
-    dealer_turn
-        break if bust? || stay
-    decide_winner
-    display_good_message
+    puts player.hand.inspect
+    puts dealer.hand.inspect
+    puts @deck.size
+  end
+
+  private
+
+  attr_reader :player, :dealer, :deck
+  # deal
+  # player_turn
+  #     break if bust? || stay
+  # dealer_turn
+  #     break if bust? || stay
+  # decide_winner
+  # display_good_message
 end
 
 # reassess what Hand and Deck should be
+
+Game_21.new.play
